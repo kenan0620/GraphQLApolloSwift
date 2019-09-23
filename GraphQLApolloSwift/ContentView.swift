2ad6370037887f8fed3b150994b0fce3f1e38300
conan <coenen@aliyun.com>
2019-09-23 15:29:43 +0800

    Xcode11 swift版本创建

---
+++ GraphQLApolloSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GraphQLApolloSwift
+//
+//  Created by 柯南 on 2019/9/23.
+//  Copyright © 2019 柯南. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ GraphQLApolloSwift/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  GraphQLApolloSwift
+//
+//  Created by 柯南 on 2019/9/23.
+//  Copyright © 2019 柯南. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+class HomeVC: UIViewController{
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.white
+        
+        print("Hello welcome to Xcode11")
+    }
+}
